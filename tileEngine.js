cea031466305ab5bcb7825d06679b840aefa6bdd
basementstudi0s <emmettwager@gmail.com>
2023-11-14 09:11:17 -0500

    Co-authored-by: Benjamin <benpai@sparty18.com>

--- tileEngine.js
+++ tileEngine.js
@@ -1,71 +1,82 @@
-var context;
-var tileWidth = 80;
-var tileHeight = 80;
-var tileDrawX;
-var tileDrawY;
-var tileDrawOffsetX;
-var tileDrawOffsety;
-var camera = [JSON.parse('{"id":"0", "x":"0", "y":"0"}')];
-var cameraMoveXVel = 0;
-var cameraMoveYVel = 0;
-var renderWidth = 14;
-var renderHeight = 10;
-var mouseX;
-var mouseY;
-var mouseCameraX;
-const tileDataWidth = 100;
-const tileDataHeight = 100;
+let
+    context,
+    tileWidth = 80,
+    tileHeight = 80,
+    tileDrawX,
+    tileDrawY,
+    tileDrawOffsetX,
+    tileDrawOffsety,
+    camera = [JSON.parse('{"id":"0", "x":"0", "y":"0"}')],
+    cameraMoveXVel = 0,
+    cameraMoveYVel = 0,
+    renderWidth = 14,
+    renderHeight = 10,
+    mouseX,
+    mouseY,
+    mouseCameraX;
+const
+    tileDataWidth = 100,
+    tileDataHeight = 100;
 //images
-const grassImage = new Image();
-grassImage.src = "grass.png";
 
-const stoneImage = new Image();
-stoneImage.src = "stone.png";
-
-const hayBaleImage = new Image();
-hayBaleImage.src = "hayBale.png";
-
-const airImage = new Image();
-airImage.src = "air.png";
-
-const dirtImage = new Image();
-dirtImage.src = "dirt.png";
-
-const logImage = new Image();
-logImage.src = "log.png";
-
-
+class Tile {
+    constructor(name, additionalData) {
+        switch (name) {
+            case 'grass':
+                break;
+            case 'air':
+                break;
+            case 'stone':
+                break;
+            case 'hayBale':
+                break;
+            case 'dirt':
+                break;
+            case 'log':
+                break;
+            default:
+                throw new SyntaxError("Not a valid tile.")
+        }
+        this.image = new Image()
+        this.image.src = `${name}.png`
+        this.name = name
+        if (additionalData) Object.entries(additionalData).forEach(([k,v]) => {this[k]= v})
+    }
+    break() {
+        console.log('breaking...')
+    }
+}
 
 // Add an event listener to the document for the mousemove event
-document.addEventListener('mousemove', function(event) {
+document.addEventListener('mousemove', function (event) {
     // Get the mouse X and Y coordinates
     mouseX = event.offsetX;
     mouseY = event.offsetY;
-    
+
 
     // Log the mouse coordinates to the console
     //console.log('Mouse X Coordinate:', mouseX);
     //console.log('Mouse Y Coordinate:', mouseY);
-  });
+});
 
-  
+
 
 
 //tile data, very important
 //datacontained in array, block data will be in json format eg). [[{},{}],[{},{}]]
 const tilePresets = {
-    "grass" : {"name" : "grass", "image": grassImage},
-    "stone" : {"name" : "stone", "image": stoneImage},
-    "hayBale" : {"name" : "hayBale", "image": hayBaleImage},
-    "air" : {"name" : "air", "image": airImage},
-    "dirt" : {"name" : "dirt", "image": dirtImage},
-    "log" : {"name" : "log", "image": logImage}
+    grass: new Tile('grass'),
+    stone: new Tile('stone'),
+    hayBale: new Tile('hayBale'),
+    air: new Tile('air'),
+    dirt: new Tile('dirt'),
+    log: new Tile('log')
 };
 var tileData = [];
 
 for (let i = 0; i < tileDataHeight; i++) {
     let genCol = [];
-    for (let i = 0; i < tileDataWidth; i++) {
+    for (let v = 0; i < tileDataWidth; v++) {
         genCol.push(tilePresets.dirt);
     }
     tileData.push(genCol);
@@ -83,7 +94,7 @@ for (let _y = 0; _y < tileDataHeight; _y++) {
 
 for (let _y = 0; _y < tileDataHeight; _y++) {
     for (let _x = 0; _x < tileDataWidth; _x++) {
-        
+
         if (tileData[_x][_y + 1] == tilePresets.air && tileData[_x][_y] != tilePresets.air) {
             tileData[_x][_y] = tilePresets.grass;
         }
@@ -92,38 +103,29 @@ for (let _y = 0; _y < tileDataHeight; _y++) {
 
 for (let _y = 0; _y < tileDataHeight; _y++) {
     for (let _x = 0; _x < tileDataWidth; _x++) {
-        
         if (tileData[_x][_y + 1] == tilePresets.air && tileData[_x][_y] != tilePresets.air && Math.random() > 0.8 && tileData[_x][_y] != tilePresets.log) {
             generateTree(_x, _y, 2, 4);
         }
     }
 }
 
-
+generateCircle(10, 10, 5, tilePresets.stone)
 
 console.log(tileData);
 
-
 let pressedKeys = [];
 
-document.addEventListener('keydown', function(event) {
-  // Check if the pressed key is a letter
-  if (event.key.length === 1) {
-    // Add the pressed key to the array if not already present
-    if (!pressedKeys.includes(event.key)) {
-      pressedKeys.push(event.key);
-    }
-  }
+document.addEventListener('keydown', function (event) {
+    if (event.key.length === 1 && !pressedKeys.includes(event.key)) pressedKeys.push(event.key);
 });
 
-document.addEventListener('keyup', function(event) {
-  // Remove the released key from the array
-  pressedKeys = pressedKeys.filter(key => key !== event.key);
+document.addEventListener('keyup', function (event) {
+    pressedKeys = pressedKeys.filter(key => key !== event.key);
 });
 
 
 //board
-window.onload = function() {
+window.onload = function () {
     board = document.getElementById("board");
     board.width = 960;
     board.height = 540;
@@ -133,30 +135,21 @@ window.onload = function() {
 }
 
 function update() {
-    cameraMoveXVel = (pressedKeys.includes("d") - pressedKeys.includes("a"))*10;
-    cameraMoveYVel = (pressedKeys.includes("s") - pressedKeys.includes("w"))*10;
+    cameraMoveXVel = (pressedKeys.includes("d") - pressedKeys.includes("a")) * 10;
+    cameraMoveYVel = (pressedKeys.includes("s") - pressedKeys.includes("w")) * 10;
     //cameraMoveXVel -= cameraMoveXVel/20;
     //cameraMoveYVel -= cameraMoveYVel/20;
-    if (Math.abs(cameraMoveXVel) < 0.4) {
-        cameraMoveXVel = 0;
-    }
-    if (Math.abs(cameraMoveYVel) < 0.4) {
-        cameraMoveYVel = 0;
-    }
-
-    getCameraById(0).x = (parseInt(getCameraById(0).x) + cameraMoveXVel);
-    getCameraById(0).y = (parseInt(getCameraById(0).y) + cameraMoveYVel);// why is it like this?!?!?!?! RESEARCH
-
-    context.fillStyle = "black";//draw board
+    if (Math.abs(cameraMoveXVel) < 0.4) cameraMoveXVel = 0;
+    if (Math.abs(cameraMoveYVel) < 0.4) cameraMoveYVel = 0;
+    getCameraById(0).x += cameraMoveXVel;
+    getCameraById(0).y += cameraMoveYVel;
+    context.fillStyle = "black";
     context.fillRect(0, 0, board.width, board.height);
-
-    let cameraBorder = positionBorder(getCameraById(0).x, getCameraById(0).y, 0, 0, (tileDataWidth * tileWidth)-board.width, -((tileDataHeight * tileHeight)-board.height));
+    let cameraBorder = positionBorder(getCameraById(0).x, getCameraById(0).y, 0, 0, (tileDataWidth * tileWidth) - board.width, -((tileDataHeight * tileHeight) - board.height));
     getCameraById(0).x = cameraBorder.x;
     getCameraById(0).y = cameraBorder.y;
-    let selectedTile = getTileDataIndex(getMouseCameraPosition(0).x, getMouseCameraPosition(0).y);
+    // let selectedTile = getTileDataIndex(getMouseCameraPosition(0).x, getMouseCameraPosition(0).y);
     drawTiles();
-    
-
 }
 
 function getCameraById(_id) {
@@ -168,23 +161,23 @@ function getCameraById(_id) {
 }
 
 function drawTiles() {
-    var tileDrawOffsetX = (Math.round(getCameraById(0).x/(tileWidth))*(tileWidth)) - getCameraById(0).x - tileWidth;// minused by tile width to avoid showing border space
-    var tileDrawOffsetY = (Math.round(getCameraById(0).y/(tileHeight))*(tileHeight)) - getCameraById(0).y + board.height;
+    let tileDrawOffsetX = (Math.round(getCameraById(0).x / (tileWidth)) * (tileWidth)) - getCameraById(0).x - tileWidth;// minused by tile width to avoid showing border space
+    let tileDrawOffsetY = (Math.round(getCameraById(0).y / (tileHeight)) * (tileHeight)) - getCameraById(0).y + board.height;
     tileDrawX = 0;
     tileDrawY = 0;
     for (let i = 0; i < renderHeight; i++) {
-        for (let i = 0; i < renderWidth; i++) {
+        for (let v = 0; v < renderWidth; i++) {
             if (tileDrawOffsetX + (tileDrawX * tileWidth) > -tileWidth && tileDrawOffsetX + (tileDrawX * tileWidth) < 960) {
-            let currentTile = tileData[tileDrawX + Math.round(getCameraById(0).x/tileWidth)-1][tileDrawY - Math.round(getCameraById(0).y/tileHeight)-1]; //DEBUG needs to check if has an image key
-            if (currentTile != undefined) {
-                if (currentTile.image != airImage) {
-                    context.drawImage(currentTile.image, tileDrawOffsetX + (tileDrawX * tileWidth), tileDrawOffsetY + (-tileDrawY * tileHeight), 80, 80);
+                let currentTile = tileData[tileDrawX + Math.round(getCameraById(0).x / tileWidth) - 1][tileDrawY - Math.round(getCameraById(0).y / tileHeight) - 1]; //DEBUG needs to check if has an image key
+                if (currentTile != undefined) {
+                    if (currentTile.image != tilePresets.air.image) {
+                        context.drawImage(currentTile.image, tileDrawOffsetX + (tileDrawX * tileWidth), tileDrawOffsetY + (-tileDrawY * tileHeight), 80, 80);
+                    }
                 }
+
+                //context.fillRect(tileDrawOffsetX + (tileDrawX * tileWidth), tileDrawOffsetY + (-tileDrawY * tileHeight), tileWidth, tileHeight);
+
             }
-            
-            //context.fillRect(tileDrawOffsetX + (tileDrawX * tileWidth), tileDrawOffsetY + (-tileDrawY * tileHeight), tileWidth, tileHeight);
-            
-        }
             tileDrawX += 1;
         }
         tileDrawX = 0;
@@ -194,7 +187,7 @@ function drawTiles() {
 
 function positionBorder(_x, _y, _borderLeft, _borderDown, _borderRight, _borderTop) {
     if (_x < _borderLeft) {
-        _x = _borderLeft;Math.round(_y / tileHeight)
+        _x = _borderLeft; Math.round(_y / tileHeight)
     }
     if (_y > _borderDown) {
         _y = _borderDown;
@@ -205,22 +198,22 @@ function positionBorder(_x, _y, _borderLeft, _borderDown, _borderRight, _borderT
     if (_y < _borderTop) {
         _y = _borderTop;
     }
-    return {"x" : _x, "y" : _y};
+    return { "x": _x, "y": _y };
 }
 
 function getTileDataIndex(_x, _y) {
-    return {"x" : Math.round((_x + 40) / tileWidth)-1, "y" : -Math.round((_y + 60) / tileHeight)+7};
+    return { "x": Math.round((_x + 40) / tileWidth) - 1, "y": -Math.round((_y + 60) / tileHeight) + 7 };
 }
 
-function getMouseCameraPosition (_camId) {
-    return {"x" : mouseX + camera[_camId].x, "y" : mouseY + camera[_camId].y};
+function getMouseCameraPosition(_camId) {
+    return { "x": mouseX + camera[_camId].x, "y": mouseY + camera[_camId].y };
 }
 
 function setTile(_x, _y, _tile, _array = tileData) {
     if (_x >= 0 && _x < tileDataWidth && _y >= 0 && _y < tileDataHeight) {
         _array[_x][_y] = _tile;
     }
-    
+
 
 }
 
@@ -240,5 +233,19 @@ function generateTree(_xOffset, _yOffset, _thickness, _iterations) {
     if (_iterations > 0) {
         generateTree(_x + _xOffset, _y + _yOffset, _thickness, _iterations - 1);
     }
-    
+
+}
+
+
+//generated by github cop
+function generateCircle(centerX, centerY, radius, tileType) {
+    for (let y = -radius; y <= radius; y++) {
+        for (let x = -radius; x <= radius; x++) {
+            // Check if the current tile is within the circle
+            if (x * x + y * y <= radius * radius) {
+                // Set the tile at the current position to the specified tile type
+                setTile(centerX + x, centerY + y, tileType);
+            }
+        }
+    }
 }
