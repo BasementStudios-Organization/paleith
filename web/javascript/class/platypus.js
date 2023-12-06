e79ee47e4fefc7b082841a5dbbd9bef2bb11cd2f
BasementStudi0s <149422128+BasementStudi0s@users.noreply.github.com>
2023-12-06 14:38:03 -0500

    entity img dim are not customizable

--- web/javascript/class/platypus.js
+++ web/javascript/class/platypus.js
@@ -3,5 +3,8 @@ const Entity = require('./entity')
 module.exports = class Platypus extends Entity {
     constructor(name, data, game) {
         super(name, data, game)
+        this.dim = Object.assign({ w: 220, h: 280 }, data.dim ?? {});
     }
+    
+
 }
